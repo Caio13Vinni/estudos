e7a9c8017292202100d76178b62dc884be8be65c
caio <caiov1nn1@hotmail.com>
2026-08-01 11:27:04 -0300

    feat: add array aula2

---
+++ java/src/academy/devdojo/maratonajava/introducao/Array07Arrays02.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Array07Arrays02 {
+    public static void main(String[] args) {
+        int[] idades = new int[3];
+        //byte, short, double, float, long, int = 0;
+        //char '\u0000' ''
+        //boolean false
+        //String null
+
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
