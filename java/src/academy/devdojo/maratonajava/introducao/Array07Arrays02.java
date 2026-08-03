8e116c05b3a9f064f0bb24bbb3f82ef329376921
caio <caiov1nn1@hotmail.com>
2026-08-03 15:48:41 -0300

    feat: add array aula3

--- java/src/academy/devdojo/maratonajava/introducao/Array07Arrays02.java
+++ java/src/academy/devdojo/maratonajava/introducao/Array07Arrays02.java
@@ -2,14 +2,19 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Array07Arrays02 {
     public static void main(String[] args) {
-        int[] idades = new int[3];
         //byte, short, double, float, long, int = 0;
         //char '\u0000' ''
         //boolean false
         //String null
 
-        System.out.println(idades[0]);
-        System.out.println(idades[1]);
-        System.out.println(idades[2]);
+        String[] names = new String[3];
+        names [0] = "goku";
+        names [1] = "naruto";
+        names [2] = "gon";
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+
+        }
     }
 }
