71b4270dbe2ab9ffe7589405568e495eddbac85d
caio <caiov1nn1@hotmail.com>
2026-08-05 21:43:26 -0300

    feat: add array Multidimensional 1

---
+++ java/src/academy/devdojo/maratonajava/introducao/class08MultidimensionalArrays02.java
@@ -0,0 +1,26 @@
+package academy.devdojo.maratonajava.introducao;
+
+import java.util.Arrays;
+
+public class class08MultidimensionalArrays01 {
+    public static void main(String[] args) {
+        int days[][] = new int[3][3];
+        days[0][0] = 31;
+        days[0][1] = 28;
+        days[0][2] = 31;
+        days[1][0] = 31;
+        days[1][1] = 28;
+        days[1][2] = 31;
+
+        for (int[] day : days){
+            System.out.println(Arrays.toString(day));
+        }
+
+        for (int i = 0; i < days.length; i++) {
+            for (int j = 0; j < days[i].length; j++) {
+                System.out.print(" [" + days[i][j] +  " ]");
+            }
+        }
+
+    }
+}
