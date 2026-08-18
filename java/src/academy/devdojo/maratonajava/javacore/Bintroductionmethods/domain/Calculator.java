721c8775c0f0f2be41f65c37788cd4b314dc5ba5
caio <caiov1nn1@hotmail.com>
2026-08-17 21:57:32 -0300

    poo: methods

---
+++ java/src/academy/devdojo/maratonajava/javacore/Bintroductionmethods/domain/Calculator.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.javacore.Bintroductionmethods.domain;
+
+public class Calculator {
+
+    public void sumTwoNumbers() {
+        System.out.println(10 + 10);
+    }
+}
