d8c6623a64f440e1fa388bdf468af199af7fff65
caio <caiov1nn1@hotmail.com>
2026-08-27 23:00:14 -0300

    poo: acess Modifier

---
+++ java/src/academy/devdojo/maratonajava/javacore/Bintroductionmethods/test/PersonTest01.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.javacore.Bintroductionmethods.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroductionmethods.domain.Person;
+
+public class PersonTest01 {
+    public static void main(String[] args) {
+        Person person = new Person();
+        /*
+                person.name = "jiraya";
+                person.age = 70;
+        */
+
+        person.setName("jiraya");
+        person.setAge(-44);
+
+        person.print();
+
+    }
+}
