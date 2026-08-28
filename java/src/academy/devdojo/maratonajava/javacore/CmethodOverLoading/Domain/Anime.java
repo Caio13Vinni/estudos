c136cd8bed6c49ba19d2def6013350d74f860988
caio <caiov1nn1@hotmail.com>
2026-08-28 16:15:27 -0300

    poo: acess Modifier part 3 and overloading

---
+++ java/src/academy/devdojo/maratonajava/javacore/CmethodOverLoading/Domain/Anime.java
@@ -0,0 +1,64 @@
+package academy.devdojo.maratonajava.javacore.CmethodOverLoading.Domain;
+
+public class Anime {
+    private String name;
+    private String type;
+    private int episodes;
+    private String status;
+
+    public void init(String name, String type, int episodes){
+        this.name = name;
+        this.type = type;
+        this.episodes = episodes;
+
+    }
+
+    public void init(String name, String type, int episodes, String status){
+        this.name = name;
+        this.type = type;
+        this.episodes = episodes;
+        this.status = status;
+    }
+
+
+
+    public void print(){
+        System.out.println(this.name);
+        System.out.println(this.type);
+        System.out.println(this.episodes);
+        System.out.println(this.status);
+    }
+
+    public void setStatus(String status){
+        this.status = status;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setEpisodes(int episodes) {
+        this.episodes = episodes;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void getEpisodes(int episodes){
+        this.episodes = episodes;
+    }
+
+    public void getType(String type){
+        this.type = type;
+    }
+
+    public void getStatus(String status){
+        this.status = status;
+    }
+}
---
+++ java/src/academy/devdojo/maratonajava/javacore/CmethodOverLoading/Test/AnimeTest01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.CmethodOverLoading.Test;
+
+import academy.devdojo.maratonajava.javacore.CmethodOverLoading.Domain.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime();
+
+        anime.init("Akuda Drive", "Tv " , 12);
+
+        anime.print();
+    }
+}
