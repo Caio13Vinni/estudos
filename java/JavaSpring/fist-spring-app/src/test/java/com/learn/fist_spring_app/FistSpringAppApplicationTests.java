0a14be80f337aae1aa1492e972e39a884b2dfa10
caio <caiov1nn1@hotmail.com>
2026-09-10 16:23:22 -0300

    Spring: hello world

---
+++ java/JavaSpring/fist-spring-app/src/test/java/com/learn/fist_spring_app/FistSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.fist_spring_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FistSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
